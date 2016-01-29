19928cc062c749a8a7eb19bb127895244a37371a
Fabien Goglio <fabien.goglio@tfnux.org>
2016-01-29 17:06:40 +0100

    remove unused functions

--- util.go
+++ util.go
@@ -12,27 +12,3 @@ func parseCommand(commandLine string) {
 	cmd.Args = args
 	cmd.Run()
 }
-
-// compare two []byte
-func testEq(a, b []byte) bool {
-	if len(a) != len(b) {
-		return false
-	}
-
-	for i := range a {
-		if a[i] != b[i] {
-			return false
-		}
-	}
-
-	return true
-}
-
-func insertRule(slice []*RoutingRule, index int, value *RoutingRule) []*RoutingRule {
-	newSlice := make([]*RoutingRule, len(slice), cap(slice)+1)
-	copy(newSlice, slice)
-	slice = newSlice[0 : len(slice)+1]
-	copy(slice[index+1:], slice[index:])
-	slice[index] = value
-	return slice
-}
